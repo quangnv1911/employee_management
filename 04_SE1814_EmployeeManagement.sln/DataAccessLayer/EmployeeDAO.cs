6fc2e8ccaace72c7ffee14d40a352a86626ee205
quangnv <quangnv1911@gmail.com>
2024-06-10 21:41:47 +0700

    create the core project

---
+++ 04_SE1814_EmployeeManagement.sln/DataAccessLayer/EmployeeDAO.cs
@@ -0,0 +1,17 @@
+﻿using _04_SE1814_EmployeeManagement.sln.BusinessObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_SE1814_EmployeeManagement.sln.DataAccessLayer
+{
+    public static class EmployeeDAO
+    {
+        public static void InsertEmployee(Employee employee)
+        {
+
+        }
+    }
+}
